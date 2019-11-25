55d6a4d49a4b66df9201ec8ffa2268095c31a115
shamilv05 <vagabov.sha@phystech.edu>
2019-11-26 00:11:29 +0300

    timeout for every hundredth request added

--- source/sender/uuidsender.go
+++ source/sender/uuidsender.go
@@ -4,11 +4,18 @@ import (
 	"../api"
 	"context"
 	"github.com/google/uuid"
+	"time"
 )
+
+var counter int = 0
 
 type GRPCServer struct {}
 
 func (p *GRPCServer) IdSend(ctx context.Context, req *api.Empty) (*api.UUID, error) {
+	counter++
+	if counter % 100 == 0 {
+		time.Sleep(2 * time.Second)
+	}
 	id, err := uuid.NewUUID()
 	return &api.UUID{Uuid: id.String()}, err
 }
